346a4de8d232814b5716045deb64517748ad89e6
daddy-warbucks <warbucks@klassifieds.com.au>
2014-01-26 20:16:17 +1100

    Finish User Sign In

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Klassifieds"
+      sign_in @user
+      flash[:success] = "Welcome to the Klassifiedss!"
       redirect_to @user
     else
       render 'new'
